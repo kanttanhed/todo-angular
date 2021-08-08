aa62acf786f274618ab52d6a84272c3563dea5a3
pliniosalazar <pliniocs@ciant.com>
2021-08-08 12:30:25 -0300

    adding read all

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -7,19 +7,29 @@ import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 import { FooterComponent } from './components/footer/footer.component';
+import { ReadAllComponent } from './components/read-all/read-all.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ReadAllComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ todo/src/app/components/read-all/read-all.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+
+
+@Component({
+  selector: 'app-read-all',
+  templateUrl: './read-all.component.html',
+  styleUrls: ['./read-all.component.css']
+})
+export class ReadAllComponent implements OnInit {
+
+  list: Todo[] =[
+    {
+      titulo: "Teste",
+      dataParaFinalizar: new Date,
+      finalizado: false
+    },
+    {
+      titulo: "Teste",
+      dataParaFinalizar: new Date,
+      finalizado: false
+    },
+    {
+      titulo: "Teste",
+      dataParaFinalizar: new Date,
+      finalizado: false
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
